5a8a88b07fb11caeeab766956428187c7fcee486
Ferdousi Haque <ferdousi160228@gmail.com>
2020-12-04 21:14:33 +0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,7 @@
 db.collection('settings').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
       var tittle_input = document.getElementById("exampleInputTittle1");
-      var heading_input = document.getElementById("exampleInputHeading1");
+      var banner_input = document.getElementById("exampleInputBanner1");
       var address_input = document.getElementById("exampleInputAddress1");
       var faddress_input = document.getElementById("exampleInputFullAddress1");
       var email_input = document.getElementById("exampleInputEmail1");
@@ -10,23 +10,23 @@ db.collection('settings').get().then((snapshot) => {
       var copyright_input = document.getElementById("exampleInputCopyright1");
   
       tittle_input.value = doc.data().tittle;
-      heading_input.value = doc.data().heading;
+      banner_input.value = doc.data().banner;
       address_input.value = doc.data().address;
       faddress_input.value = doc.data().fullAddress;
       email_input.value = doc.data().email;
       mobile_input.value = doc.data().mobile;
       copyright_input.value = doc.data().copyright;
-      updateAbout(doc.id);
+      updateSettings(doc.id);
       });
   }).catch(function(error) {
       console.log("Error getting document:", error);
   });
   
   // update settings data after clicking update button of dashboard.html
-  function updateAbout(id){
+  function updateSettings(id){
   
       var tittle_input = document.getElementById("exampleInputTittle1");
-      var heading_input = document.getElementById("exampleInputHeading1");
+      var banner_input = document.getElementById("exampleInputBanner1");
       var address_input = document.getElementById("exampleInputAddress1");
       var faddress_input = document.getElementById("exampleInputFullAddress1");
       var email_input = document.getElementById("exampleInputEmail1");
@@ -39,7 +39,7 @@ db.collection('settings').get().then((snapshot) => {
         e.preventDefault();
         db.collection("settings").doc(id).update({ 
           tittle: tittle_input.value,
-          heading: heading_input.value,
+          banner: banner_input.value,
           address: address_input.value,
           fullAddress: faddress_input.value,
           email: email_input.value,
@@ -58,5 +58,3 @@ db.collection('settings').get().then((snapshot) => {
       });
   
   }
-  
-  
