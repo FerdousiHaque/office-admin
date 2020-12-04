7347cad2f9db922abbe6dab63f9ff7e70e8a8b51
Ferdousi Haque <ferdousi160228@gmail.com>
2020-12-04 13:34:19 +0600

    Add files via upload

---
+++ js/dashboard.js
@@ -0,0 +1,62 @@
+//dashboard.html --> put settings data in all input fields
+db.collection('settings').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+      var tittle_input = document.getElementById("exampleInputTittle1");
+      var heading_input = document.getElementById("exampleInputHeading1");
+      var address_input = document.getElementById("exampleInputAddress1");
+      var faddress_input = document.getElementById("exampleInputFullAddress1");
+      var email_input = document.getElementById("exampleInputEmail1");
+      var mobile_input = document.getElementById("exampleInputMobile1");
+      var copyright_input = document.getElementById("exampleInputCopyright1");
+  
+      tittle_input.value = doc.data().tittle;
+      heading_input.value = doc.data().heading;
+      address_input.value = doc.data().address;
+      faddress_input.value = doc.data().fullAddress;
+      email_input.value = doc.data().email;
+      mobile_input.value = doc.data().mobile;
+      copyright_input.value = doc.data().copyright;
+      updateAbout(doc.id);
+      });
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+  
+  // update settings data after clicking update button of dashboard.html
+  function updateAbout(id){
+  
+      var tittle_input = document.getElementById("exampleInputTittle1");
+      var heading_input = document.getElementById("exampleInputHeading1");
+      var address_input = document.getElementById("exampleInputAddress1");
+      var faddress_input = document.getElementById("exampleInputFullAddress1");
+      var email_input = document.getElementById("exampleInputEmail1");
+      var mobile_input = document.getElementById("exampleInputMobile1");
+      var copyright_input = document.getElementById("exampleInputCopyright1");
+  
+      var updateSettings = document.getElementById("updateSettings");
+  
+      updateSettings.addEventListener('click', (e) => { // Update data into Firebase
+        e.preventDefault();
+        db.collection("settings").doc(id).update({ 
+          tittle: tittle_input.value,
+          heading: heading_input.value,
+          address: address_input.value,
+          fullAddress: faddress_input.value,
+          email: email_input.value,
+          mobile: mobile_input.value,
+          copyright: copyright_input.value,
+        })
+        .then(function () {
+          alert("Yeah Updated!!");
+          location.reload();  //reload to load all the product
+          return false;
+        })
+        .catch(function (error) {
+          console.log('Update failed: ' + error.message);
+        });
+  
+      });
+  
+  }
+  
+  
